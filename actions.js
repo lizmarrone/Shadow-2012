d01232141f87a10d24426315a6c41c1142ec1154
Jordan Jacobs <jacobsinfotech@gmail.com>
2012-12-04 11:23:18 -0800

    Updated coding and styling to reflect on PSD and site functionality.

--- actions.js
+++ actions.js
@@ -1,84 +1,5 @@
 		$(document).ready(function()
 		{
-			/*$("#accordion").accordion();
-			$("button")
-            .button()
-            .click(function( event ) {*/
-                /*event.preventDefault();*/
-            /*    $("#dialog").dialog();
-            });
-            $("#datepicker").datepicker();*/
-            
-            $("div#uiButtonBar div#radio").buttonset();
-            $("input#datepicker").datepicker();
-            $("div#uiButton button").button();
-            $("#tabs").tabs();
-            $("#slider").slider();
-            $("div#uiLinkButton a").button();  
-            $( "#uiActiveProgressBar" ).progressbar({
-            	value: 59
-        	});               
-            
-			var spinner = $( "#spinner" ).spinner();
-	 
-			$( "#disable" ).click(function() {
-				if ( spinner.spinner( "option", "disabled" ) ) {
-					spinner.spinner( "enable" );
-				} else {
-					spinner.spinner( "disable" );
-				}
-			});
-			$( "#destroy" ).click(function() {
-				if ( spinner.data( "ui-spinner" ) ) {
-					spinner.spinner( "destroy" );
-				} else {
-					spinner.spinner();
-				}
-			});
-			$( "#getvalue" ).click(function() {
-				alert( spinner.spinner( "value" ) );
-			});
-			$( "#setvalue" ).click(function() {
-				spinner.spinner( "value", 5 );
-			});
-            
-             $("div#uiComboBox select").mouseover(function()
-            {
-            	$(this).css("border","1px solid #2cabb8");
-            });
-            $("div#uiComboBox select").mouseout(function()
-            {
-            	$(this).css("border","1px solid #000000");
-            });               
-            			
-            $("div#uiTextArea textarea").focus(function()
-            {
-            	$(this).css("border","2px solid #2cabb8");
-            });
-            $("div#uiTextArea textarea").focusout(function()
-            {
-            	$(this).css("border","2px solid #000000");
-            });
-            
-            $("div#uiTextInput input").focus(function()
-            {
-            	$(this).css("border","2px solid #2cabb8");
-            });
-            $("div#uiTextInput input").focusout(function()
-            {
-            	$(this).css("border","2px solid #000000");
-            });
-            
-            $("a.ui-spinner-button").mouseover(function()
-            {
-            	$("a.ui-spinner-button").css("border","1px solid #2cabb8");
-            });
-            
-            $("a.ui-spinner-button").mouseout(function()
-            {
-            	$("a.ui-spinner-button").css("border","1px solid #000000");
-            });              
-            
 			$("div#search input#txtSearch").click(function()
 			{
 				if($(this).val() == "Enter Search Keyword Here")
@@ -128,5 +49,7 @@
 				{
 					$(this).val("Full Name");
 				}				
-			});				
+			});		
+			
+				
 		});
